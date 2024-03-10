44cda3c1432cb1fc56596b08110cd470ad0bd998
uznEnehC <noviachin@gmail.com>
2024-03-10 16:37:41 +0800

    Update diy-part1.sh

--- test/diy-part1.sh
+++ test/diy-part1.sh
@@ -17,5 +17,10 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git haibo https://github.com/haiibo/openwrt-packages' >>feeds.conf.default
+#echo 'src-git liuran001 https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
+
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
