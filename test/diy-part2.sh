d6e9c8c6f2eaf9f80d7673ccce11bce376810ebd
uznEnehC <noviachin@gmail.com>
2024-03-10 22:44:26 +0800

    Update diy-part2.sh

--- test/diy-part2.sh
+++ test/diy-part2.sh
@@ -20,4 +20,4 @@ sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/M
 #sed -i '/uci commit system/i\uci set system.@system[0].hostname='LEDE'' package/lean/default-settings/files/zzz-default-settings
  
 # 加入编译者信息（需要取消下面的注释）
-sed -i "s/OpenWrt /Github build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /LEDE build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
