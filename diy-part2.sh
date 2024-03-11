3de732720f28a226ea5473e4c6d71a1ec5d9fd76
uznEnehC <noviachin@gmail.com>
2024-03-11 22:56:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,11 +13,13 @@
 # 修改默认 IP
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
+# 使用 jerrykuku/luci-theme-argon v1.x.x 主题
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-v1
 # 修改默认主题
-sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
+sed -i "s/luci-theme-bootstrap/luci-theme-argon-v1/g" feeds/luci/collections/luci/Makefile
  
 # 修改默认主机名（需要取消下面的注释）
 #sed -i '/uci commit system/i\uci set system.@system[0].hostname='LEDE'' package/lean/default-settings/files/zzz-default-settings
  
-# 加入编译者信息（需要取消下面的注释）
-#sed -i "s/OpenWrt /Github Action build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+# 加入编译者信息
+sed -i "s/OpenWrt /LEDE build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
